218d348b990a219aacedb85a4910a3df1c01eccf
Michał Krakowiak <michal.krak97@gmail.com>
2018-03-18 10:42:35 +0100

    Feed forward neural layer

--- AI/neural_layer.py
+++ AI/neural_layer.py
@@ -1,5 +1,21 @@
 import numpy as np
 
 class NerualLayer():
-    def __init__(self):
+    def __init__(self, inputs, outputs, activation=None, prev=None):
+        self._matrix = 2* np.random.random((inputs, outputs)) - 1
+        if prev is not None:
+            prev._next = self
+        self._prev = prev
+        self._next = None
+        self._activation = activation
+    
+    def feed_forward(self, input):
+        output = np.dot(input, self._matrix)
+        if self._activation is not None:
+            output = self._activation(output)
+        if self._next is not None:
+            output = self._next.feed_forward(output)
+        return output
+    
+    def backpropagation(self):
         pass
