2f976e6e218e34c14e4c309afc2fbe504b143d5a
Michał Krakowiak <michal.krak97@gmail.com>
2018-03-18 17:46:21 +0100

    backpropagation

--- AI/nerualnet.py
+++ AI/nerualnet.py
@@ -1,15 +1,18 @@
 from neural_layer import NerualLayer
-
+from activation import Linear
+import numpy as np
 class NerualNet():
     def __init__(self, shape): #shape is a list of sizes for neural layers
-        self._first = NerualLayer(shape[0], shape[1])
+        self._first = NerualLayer(inputs=shape[0], outputs=shape[1])
         tmp = self._first
         for i in range(1, len(shape) - 1):
-            self._last = NerualLayer(shape[i], shape[i+1], prev=tmp)
+            self._last = NerualLayer(inputs=shape[i], outputs=shape[i+1], prev=tmp)
             tmp = self._last
+        self._last._activation = Linear()
 
-    def feed_forward(self, input):
-        return self._first.feed_forward(input)
+    def predict(self, input):
+        return self._first.feed_forward(input)[0]
 
-    def backpropagate(self):
-        pass
+    def train(self, error):
+        self._last.backpropagate(error)
+
--- AI/neural_layer.py
+++ AI/neural_layer.py
@@ -1,21 +1,27 @@
+from activation import ReLu
 import numpy as np
 
 class NerualLayer():
-    def __init__(self, inputs, outputs, activation=None, prev=None):
-        self._matrix = 2* np.random.random((inputs, outputs)) - 1
+    def __init__(self, inputs, outputs, prev=None):
+        self._weights = 2* np.random.random((inputs, outputs)) - 1
         if prev is not None:
             prev._next = self
+        self._memory = None
         self._prev = prev
         self._next = None
-        self._activation = activation
-    
+        self._activation = ReLu()
+
     def feed_forward(self, input):
-        output = np.dot(input, self._matrix)
-        if self._activation is not None:
-            output = self._activation(output)
+        output = self._activation(np.dot(input, self._weights))
+        self._memory = (input, output)
         if self._next is not None:
             output = self._next.feed_forward(output)
         return output
-    
-    def backpropagation(self):
-        pass
+
+    def backpropagate(self, error):
+        input, output = self._memory
+        delta = error * self._activation.derivative(output)       
+        if self._prev is not None:
+            error_prev = delta.dot(self._weights.T)
+            self._prev.backpropagate(error_prev)
+        self._weights += np.dot(input.T, delta)
