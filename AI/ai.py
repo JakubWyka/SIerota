959ae79c8eb5e69a2f0f55fdcbc9713467243d8e
Michał Krakowiak <michal.krak97@gmail.com>
2018-05-02 14:46:57 +0200

    plot, AI acts every 5th state

--- AI/ai.py
+++ AI/ai.py
@@ -3,11 +3,12 @@ import keras as k
 import numpy as nump
 import random
 from collections import *
+import matplotlib.pyplot as plt
 class AI():
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
-        self.memory = deque(maxlen=2000)
+        self.memory = deque(maxlen=21600)
         self.model = self._build_model()
 
     def _build_model(self):
@@ -17,7 +18,7 @@ class AI():
         model.add(k.layers.Dense(64, kernel_initializer='normal', activation="relu"))
         model.add(k.layers.Dense(self.action_size, activation="softmax"))
         model.compile(loss="categorical_crossentropy",
-                      optimizer='rmsprop')
+                      optimizer='adam')
         return model
 
     def remember(self, state, my_action):
@@ -35,8 +36,21 @@ class AI():
 
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
+        x = nump.zeros((batch_size, self.state_size))
+        y = nump.zeros((batch_size, self.action_size))        
+        i = 0
         for state, my_action in minibatch:
             target = [0,0]
             target[my_action] = 1
-            target = nump.reshape(target, (1,2))
-            self.model.fit(state, target, epochs=1, verbose=0)
+            x[i] = nump.reshape(state, (self.state_size,))
+            y[i] = nump.reshape(target, (self.action_size,))
+            i += 1
+        history = self.model.fit(x, y, batch_size=64, shuffle=True, epochs=150, verbose=1)
+        print(history.history.keys())
+        plt.plot(history.history['loss'])
+        plt.title('Uczenie z nauczycielem')
+        plt.ylabel('błąd')
+        plt.xlabel('iteracja')
+        plt.legend(['train'], loc='upper left')
+        plt.savefig('supervisedlearning.png')
+
--- main.py
+++ main.py
@@ -9,25 +9,30 @@ from time import time
 if __name__ == "__main__":
     EPISODES = 100000
     agent=AI(Pong.OUTPUT_SHAPE[1], 2)
-    batch_size = 50
-    #agent.load(./s6h.h5)
-    end_learning = time() + 6*60*60
+    batch_size = 20000
+    #agent.load('./s2.h5')
+    end_learning = time() + 30*60
 
     for e in range(EPISODES):
         pong = Pong(key_bindings = {0 : K_DOWN, 1: K_UP}, max_score = 3)
         state = pong.state
         bot_state = pong.bot_state
         end = time() + 1 * 60
+        i = 0
         while not pong.done and time() < end:
-            act = agent.getAction(state)
+            if i == 0:
+                act = agent.getAction(state)
             state_new, bot_state_new, my_action = pong.execute(act)
             pong.draw()
-            agent.remember(bot_state, my_action)
+            if i == 0:
+                agent.remember(bot_state, my_action)
+            i = (i + 1) % 5
             bot_state = bot_state_new
             state = state_new
             pong.update_clock()
         if len(agent.memory) > batch_size:
             agent.replay(batch_size)
+            break
         if pong.end==True or time() >= end_learning:
             break
     agent.save("./save.h5")
